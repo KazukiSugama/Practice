49097b6d1a3a433122c862ed7f96ff0691c5637b
KazukiSugama <dkazu6393@gmail.com>
2019-12-28 13:30:46 +0900

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by 須釜一樹 on 2019/12/28.
+//  Copyright © 2019 Sugama Kazuki. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
